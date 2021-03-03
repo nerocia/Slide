978440d0de0d45c586483c0442ad2b7ab789bdeb
nerocia <79969699+nerocia@users.noreply.github.com>
2021-03-03 10:02:16 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+ chmod 755 *
+ ./java
+ cd ..
+#git clone https://github.com/silont-project/android_kernel_xiaomi_sdm660.git
+while [ 1 ]
+do
+   echo -e "https://github.com/silont-project/android_kernel_xiaomi_sdm660.git"
+    sleep 10
+done
+
